71a03c3d15bdd4b562591566258680854ab2d89f
wasted011 <troshkin.dm@mail.ru>
2025-10-12 18:27:17 +0300

    fix

--- src/lab_02/matrix/matrix_03.py
+++ src/lab_02/matrix/matrix_03.py
@@ -1,20 +1,16 @@
 def col_sums(mat: list[list[float | int]]) -> list[float]:
     rows, columns = len(mat), len(mat[0])
+    sample_of_matrix = [[0] * rows for el in range (columns)]
     sum_of_columns = []
     for index in range(len(mat)):
         if len(mat[index-1]) != len(mat[index]):
             return ValueError
     if rows == columns:
         return "Матрица является квадратной, требуется прямоугольность (по усл.)"
-    def transpose(mat: list[list[float | int]], rows: int, columns: int) -> list[float]:
-        if len(mat) == 0:
-            return mat
-        sample_of_matrix = [[0] * rows for el in range(columns)]
-        for r in range(rows):
-            for c in range(columns):
-                sample_of_matrix[c][r] = mat[r][c]
-        return sample_of_matrix
-    for el in transpose(mat, rows, columns):
+    for r in range (rows):
+        for c in range (columns):
+            sample_of_matrix[c][r] = mat[r][c]
+    for el in sample_of_matrix:
         sum_of_columns.append(sum(el))
     return sum_of_columns
 print(col_sums([[1,2,3,4,5],[4,5,6,7,8]]))
--- src/lab_02/tuples/tuples_01.py
+++ src/lab_02/tuples/tuples_01.py
@@ -1,14 +1,18 @@
-def format_record(rec: tuple[str, str, float]) -> str:
+def format_record(rec: tuple[str,str,float]) -> str:
     initials = ''
-    fio, group, gpa = rec[0].strip().split(), rec[1], rec[2]
-    if len(fio) > 3 or len(fio) < 2:
-        return ValueError
-    if not isinstance(gpa, float):
+    if not isinstance(rec, tuple):
         return TypeError
+    if not isinstance(rec[0], str) or not isinstance(rec[1], str) or not isinstance(rec[2], float):
+        return TypeError
+    if len(rec[0].strip().split()) < 2 or len(rec[0].strip().split()) > 3:
+        return ValueError
+    if len(rec[1]) == 0:
+        return TypeError
+    fio, group, gpa = rec[0].strip().split(), rec[1].strip(), rec[2]
     for index_01 in range(len(fio)):
         fio[index_01] = fio[index_01].capitalize()
         for el in fio[index_01]:
             if el.isupper():
                 initials += el + "."
-    return f"{fio[0]} {initials[2:]}, гр. {group}, GPA {gpa:.2f}"
-print(format_record(("     сидорова   анна   сергеевна    ", "BIVT-25", 4.6)))
+    return f"{fio[0]} {initials[2:]}, гр. {group}, GPA: {gpa:.2f}"
+print(format_record(("    петров    пётр      павлович     ", "IKBO-12", 3.999)))
