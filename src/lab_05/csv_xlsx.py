afb6a8767628030d1599cb2ae95a4018ba561870
wasted011 <158275147+wasted011@users.noreply.github.com>
2025-11-18 12:03:46 +0300

    fix

--- src/lab_05/csv_xlsx.py
+++ src/lab_05/csv_xlsx.py
@@ -6,17 +6,19 @@ from lib import *
 
 def csv_to_xlsx(csv_path: str | Path, xlsx_path: str | Path) -> None:
     csv_path, xlsx_path = Path(csv_path), Path(xlsx_path)
-    try:
-        wb = Workbook()
-        ws = wb.active
-        ws.title = "Sheet_1"
-        with csv_path.open(encoding='utf-8-sig') as file:
-            for element in csv.reader(file):
-                ws.append(element)
-        wb.save('data/lab_05/csv_to_xlsx.xlsx')
-        return "Успешно"
-    except (csv_path.stat().st_size or xlsx_path.stat().st_size) == 0:
-        raise ValueError
-    except not csv_path.exists() or not xlsx_path.exists():
-        raise FileNotFoundError
+    if csv_path.suffix == ".csv" and xlsx_path.suffix == ".xlsx":
+        try:
+            wb = Workbook()
+            ws = wb.active
+            ws.title = "Sheet_1"
+            with csv_path.open(encoding='utf-8-sig') as file:
+                for element in csv.reader(file):
+                    ws.append(element)
+            wb.save('data/lab_05/csv_to_xlsx.xlsx')
+            return "Успешно"
+        except (csv_path.stat().st_size or xlsx_path.stat().st_size) == 0:
+            raise ValueError
+        except not csv_path.exists() or not xlsx_path.exists():
+            raise FileNotFoundError
+    return "Неверный формат файла"
 print(csv_to_xlsx('data/lab_05/csv_to_xlsx.csv', 'data/lab_05/csv_to_xlsx.xlsx'))
