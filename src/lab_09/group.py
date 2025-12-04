586e690034eb3f84d3eb7f45d908b9cc0c44c738
wasted011 <158275147+wasted011@users.noreply.github.com>
2025-12-04 17:31:40 +0300

    update + fix

--- src/lab_09/group.py
+++ src/lab_09/group.py
@@ -4,72 +4,71 @@ from pathlib import *
 sys.path.append('src')
 from _functions_ import *
 
-class Group:
-    
-    def __init__(self, csv_file: str | Path = "data/lab_09/group.csv", students_list = []):
-        self.csv_file = csv_file
-        self.students_list = students_list
+class group:
 
-    def list(self):
-        if len(self.students_list) != 0:
-            for element in self.students_list:
-                print(f"Фио: {element["fio"]}, Дата рождения: {element["birthdate"]}, Группа: {element["group"]}, GPA: {element["gpa"]}")
-        else:
-            return "Список пуст"
+    def __init__(self, csv_file: str | Path = "data/lab_09/group.csv", json_file: str | Path = "data/lab_09/group.json", student_list: list = []):
+        self.csv_file = csv_file
+        self.json_file = json_file
+        self.student_list = student_list
+   
+    def students_list(self):
+        if is_not_empty(self.csv_file):
+            print("------------")
+            for element in self.student_list:
+                print(f"Фио: {element["fio"]}, Группа: {element["group"]}, Дата рождения: {element["birthdate"]}, GPA: {element["gpa"]}")
+            return "------------"
+        elif not is_not_empty(self.csv_file):
+            return "Файл пуст"
     
     def add_student(self, fio: str, birthdate: str, group: str, gpa: float):
         new_student = {"fio": fio, "birthdate": birthdate, "group": group, "gpa": gpa}
-        if len(self.students_list) != 0:
-            for element in self.students_list:
+        if len(self.student_list) != 0:
+            for element in self.student_list:
                 if element["fio"] != fio:
-                    self.students_list.append(new_student)
-                    write_csv_lib([values for keys, values in element.items()], self.csv_file, [keys for keys, values in element.items()])
+                    self.student_list.append(new_student)
+                    write_json(self.student_list, self.json_file)
+                    json_to_csv(self.json_file, self.csv_file)
                     return "Успешное добавление"
-                else:
-                    return "Студент уже существует"
-        else:
-            self.students_list.append(new_student)
-            for element in self.students_list:
-                write_csv_lib([values for keys, values in element.items()], self.csv_file, [keys for keys, values in element.items()])
-                return "Успешное добавление"
-
+            return "Студент уже существует"
+        self.student_list.append(new_student)
+        write_json(self.student_list, self.json_file)
+        json_to_csv(self.json_file, self.csv_file)
+        return "Успешное добавление"
+    
     def find_student(self, fio: str):
-        if len(self.students_list) != 0:
-            for element in self.students_list:
+        if len(self.student_list) != 0:
+            for element in self.student_list:
                 if element["fio"] == fio:
                     return f"Фио: {element["fio"]}, Дата рождения: {element["birthdate"]}, Группа: {element["group"]}, GPA: {element["gpa"]}"
-                else:
-                    return "Студент не найден"
+            return "Фио не найдено"
         else:
             return "Список пуст"
     
     def remove_student(self, fio: str):
-        if len(self.students_list) != 0:
-            for element in self.students_list:
+        if len(self.student_list) != 0:
+            for element in self.student_list:
                 if element["fio"] == fio:
-                    self.students_list.remove(element)
-                    write_csv_lib([values for keys, values in element.items()], self.csv_file)
+                    self.student_list.remove(element)
+                    write_json(self.student_list, self.json_file)
+                    json_to_csv(self.json_file, self.csv_file)
                     return "Успешное удаление"
-                else:
-                    return "Студент не найден"
-        else:
-            return "Список пуст"
-    
+            return "Фио не найдено"
+        return "Список пуст"
+        
     def update_student(self, fio: str):
-        updated_info = {"fio": fio, "birthdate": input("Введите дату рождения: "), "group": input("Введите наименование группы: "), "gpa": input("Введите GPA: ")}
-        if len(self.students_list) != 0:
-            for element in self.students_list:
-                if element["fio"] == fio:
-                    self.students_list.remove(element)
-                    self.students_list.append(updated_info)
-                    write_csv_lib([values for keys, values in element.items()], self.csv_file, header = None)
-                    return "Информация обновлена"
-                else:
-                    return "Студент не найден"
-        else:
-            return "Список пуст"
+        updated_student = {"fio": fio, "birthdate": input(": "), "group": input(": "), "gpa": input(": ")}
+        if len(self.student_list) != 0:
+            for index in range(len(self.student_list)):
+                if self.student_list[index]["fio"] == fio:
+                    self.student_list.remove(self.student_list[index])
+                    self.student_list.insert(index, updated_student)
+                    write_json(self.student_list, self.json_file)
+                    json_to_csv(self.json_file, self.csv_file)
+                    return "Успешное обновление"
+            return "Фио не найдено"
+        return "Список пуст"
     
-    def _read_all(self):
-        return read_text(self.csv_file)
-
-assert Group().add_student("evgeny", "2000", "bivt", 4.6) == "Успешное добавление"
+    def read_all(self):
+        if is_not_empty(self.csv_file):
+            return read_text(self.csv_file)
+        return "Файл пуст"
